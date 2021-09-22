01b45e117d3f70590feb6ffdd72e8f5c086c1d4d
Blue <leanefelis@gmail.com>
2021-09-22 13:45:17 +0200

    creation pages

---
+++ TD2/Session/accueil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> Bienvenue à l'accueil </h1>
+    <br>
+    <a href="page1.php">Page 1</a>
+    <a href="page2.php">Page 2</a>
+</body>
+
+</html>
