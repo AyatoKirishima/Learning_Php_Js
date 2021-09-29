cec30f96867218c57cafa36b9caccc2e15470335
Blue <leanefelis@gmail.com>
2021-09-29 09:15:34 +0200

    article php def variables et methodes

---
+++ TD3/Application_Objet/article.php
@@ -0,0 +1,43 @@
+<?php 
+    public class Article() {
+        private $designation, $ref, $prixUnit; //Référence,prix Unitaire
+    
+        function __constructor($designation, $ref, $prixUnit){
+            $this->designation = $designation;
+            $this->ref = $ref;
+            $this->prixUnit = $prixUnit;
+        }
+
+        // Prix
+
+        function SetPrix($prixUnit) {
+            $this->prixUnit = $prixUnit;
+        }
+
+        function GetPrix($prixUnit) {
+            return $this->prixUnit;
+        }
+
+        // Désignation
+
+        function SetDesignation($designation) {
+            $this->designation = $designation;
+        }
+
+        function GetDesignation($designation) {
+            return $this->designation;
+        }
+
+        // Référence
+
+        function SetReference($ref) {
+            $this->ref = $ref;
+        }
+
+        function GetReference($ref) {
+            return $this->ref;
+        }
+    
+        // Afficher
+    }
+?>
