d4ebdaddf2a432d88c032b2cb2609b725215eae2
Blue <leanefelis@gmail.com>
2021-10-06 15:25:20 +0200

    ajout sql début code

--- TD4/ajout.php
+++ TD4/ajout.php
@@ -1,1 +1,14 @@
 <!-- Ajouter un site à la table Sites -->
+
+<?php
+    include_once "connexion.php";
+    include_once "sites.php";
+
+    // Préparation insertion
+    $insert_stmt = $objPdo->prepare("INSERT INTO TestPrep (id,ligne) VALUES( ? , ? ) ");
+
+    $insert_stmt->bindValue(1, $nb, PDO::PARAM_STR);
+    $insert_stmt->bindValue(2, $maligne, PDO::PARAM_STR);
+    $insert_stmt->execute();
+
+?>
