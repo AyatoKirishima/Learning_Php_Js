ddbf1307e2c0ac3cda82cfd091c8f8f4a54272f1
Blue <leanefelis@gmail.com>
2021-10-13 11:10:37 +0200

    ajout a la bdd op

--- TD4/ajout.php
+++ TD4/ajout.php
@@ -15,38 +15,41 @@
     include_once "connexion.php";
     include_once "sites.php";
 
-    // Préparation insertion
-    /*$insert_stmt = $objPdo->prepare("INSERT INTO TestPrep (id,ligne) VALUES( ? , ? ) ");
+    if (array_key_exists('nom', $_POST) && array_key_exists('ville', $_POST) && array_key_exists('latitude', $_POST) && array_key_exists('longitude', $_POST)) {
+        $nom = $_POST['nom'];
+        $ville = $_POST['ville'];
+        $latitude = $_POST['latitude'];
+        $longitude = $_POST['longitude'];
 
-        $insert_stmt->bindValue(1, $row["idSite"], PDO::PARAM_STR);
-        $insert_stmt->bindValue(2, $maligne, PDO::PARAM_STR);
-        $insert_stmt->execute();*/ 
+        // Préparation insertion
+        $insert_stmt = $objPdo->prepare("INSERT INTO Sites (Nom,Ville,Latitude,Longitude) VALUES( ? , ? , ? , ? )");
 
-        if (array_key_exists('nom',$_POST) && array_key_exists('ville',$_POST) && array_key_exists('latitude',$_POST) && array_key_exists('longitude',$_POST)) {
-            $nom=$_POST['nom'];        
-            $ville=$_POST['ville'];
-            $latitude=$_POST['latitude'];
-            $longitude=$_POST['longitude'];
-        }
-        
+        $insert_stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        $insert_stmt->bindValue(2, $ville, PDO::PARAM_STR);
+        $insert_stmt->bindValue(3, $latitude, PDO::PARAM_STR);
+        $insert_stmt->bindValue(4, $longitude, PDO::PARAM_STR);
+        $insert_stmt->execute();
+    }
 
-    
+
+
     ?>
     <br>
-    <label for="nom">Nom :</label>
-    <input type="text" required name="nom">
-    <br>
-    <label for="ville">Ville :</label>
-    <input type="text" required name="ville">
-    <br>
-    <label for="latitude">Latitude :</label>
-    <input type="number" required name="latitude">
-    <br>
-    <label for="longitude">Longitude :</label>
-    <input type="number" required name="longitude">
-    <br><br>
-    <input type="submit" value="Ajouter" name="Ajouter">
-
+    <form action="" method="POST">
+        <label for="nom">Nom :</label>
+        <input type="text" required name="nom">
+        <br>
+        <label for="ville">Ville :</label>
+        <input type="text" required name="ville">
+        <br>
+        <label for="latitude">Latitude :</label>
+        <input type="number" required name="latitude">
+        <br>
+        <label for="longitude">Longitude :</label>
+        <input type="number" required name="longitude">
+        <br><br>
+        <input type="submit" value="Ajouter" name="Ajouter">
+    </form>
 </body>
 
 </html>
